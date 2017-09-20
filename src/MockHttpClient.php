654c46a6562f1abf1b49e3226270216a99d8434f
Ioannis Papikas <papikas.ioan@gmail.com>
2017-09-20 17:38:25 +0300

    ESAT-484: Add class signatures and update namespaces

--- src/MockHttpClient.php
+++ src/MockHttpClient.php
@@ -1,6 +1,15 @@
 <?php
 
-namespace HttpAdapter;
+/*
+ * This file is part of the Esat Http Package.
+ *
+ * (c) e-satisfaction Developers <tech@e-satisfaction.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Esat\Http;
 
 use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
