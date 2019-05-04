7fcd40ca4746ef8b2a808a15df6a2185ed249061
Ioannis Papikas <papikas.ioan@gmail.com>
2019-05-04 10:58:25 +0300

    Add init.php

---
+++ init.php
@@ -0,0 +1,16 @@
+<?php
+
+// Models
+require_once __DIR__ . '/src/Support/Model/BaseModel.php';
+require_once __DIR__ . '/src/Multipart/File.php';
+
+// Interfaces
+require_once __DIR__ . '/src/HttpClientInterface.php';
+require_once __DIR__ . '/src/Auth/AuthProviderInterface.php';
+
+// Abstracts
+require_once __DIR__ . '/src/AbstractHttpClient.php';
+
+// Clients
+require_once __DIR__ . '/src/HttpClient.php';
+require_once __DIR__ . '/src/AuthClient.php';
