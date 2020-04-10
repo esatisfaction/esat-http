12776ad2ca91e3b6e42b7f9b5b00e00d484faf0d
Ioannis Papikas <papikas.ioan@gmail.com>
2020-04-10 13:19:43 +0300

    Create Base Services in a layered structure

---
+++ src/BaseService.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the Esat Http Package.
+ *
+ * (c) e-satisfaction Developers <tech@e-satisfaction.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Esat\Http;
+
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class BaseService
+ * @package Esat\Support\Services
+ */
+abstract class BaseService
+{
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * BaseService constructor.
+     *
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @return LoggerInterface
+     */
+    public function getLogger()
+    {
+        return $this->logger;
+    }
+
+    /**
+     * @param LoggerInterface $logger
+     *
+     * @return $this
+     */
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+
+        return $this;
+    }
+}
